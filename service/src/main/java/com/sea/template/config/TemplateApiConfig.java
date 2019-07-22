a6a19496889e21ea34d4d0aaabe5da7abd7b80be
hychen17 <chytom01@gmail.com>
2019-07-23 00:17:46 +0800

    SEA-5: hikari and jdbc

--- service/src/main/java/com/sea/template/config/TemplateApiConfig.java
+++ service/src/main/java/com/sea/template/config/TemplateApiConfig.java
@@ -1,14 +1,18 @@
 package com.sea.template.config;
 
 import com.sea.template.api.TemplateApi;
+import com.sea.template.guest.JDBCGuestRepo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @Configuration
+@Import({GuestConfig.class})
 public class TemplateApiConfig {
 
     @Bean
-    public TemplateApi templateApi() {
-        return new TemplateApi();
+    public TemplateApi templateApi(JDBCGuestRepo repo) {
+        return new TemplateApi(repo);
     }
 }
